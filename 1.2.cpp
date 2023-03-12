d004e115faf494d57339567af83bfa724f4daac6
AnotherOneBitesTheDust5796 <118135556+AnotherOneBitesTheDust5796@users.noreply.github.com>
2023-03-12 14:22:14 +0300

    Update 1.2.cpp

--- 1.2.cpp
+++ 1.2.cpp
@@ -4,23 +4,20 @@
 using namespace std;
 
 int main() {
-	// Исходная матрица
 	vector >> matrix{{1, 2, 3},
 					{4, 5, 6},
 					{7, 8, 9}};
 
-	int numRows = matrix.size(); // Количество строк
-	int numCols = matrix[0].size(); // Количество столбцов
-
-	// Преобразование матрицы
+	int numRows = matrix.size(); 
+	int numCols = matrix[0].size();
+	
 	for (int i = 0; i < numRows; i++) {
-		int secondElement = matrix[i][1]; // Второй элемент в текущей строке
+		int secondElement = matrix[i][1];
 		for (int j = 0; j < numCols; j++) {
-			matrix[i][j] *= secondElement; // Умножение элемента на второй элемент строки
+			matrix[i][j] *= secondElement;
 		}
 	}
 
-	// Вывод преобразованной матрицы
 	for (int i = 0; i < numRows; i++) {
 		for (int j = 0; j < numCols; j++) {
 			cout << matrix[i][j] << " ";
